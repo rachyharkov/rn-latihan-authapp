dc434f48e8550baa0ce0a7b1ca8f54d714304eba
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-14 17:58:46 +0700

    manage token and autologin

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext({
   token: '',
@@ -11,12 +13,15 @@ function AuthContextProvider({children}) {
 
   const [authToken, setAuthToken] = useState()
 
+
   function authenticate(token) {
     setAuthToken(token)
+    AsyncStorage.setItem('token', token)
   }
 
   function logout() {
     setAuthToken(null)
+    AsyncStorage.removeItem('token')
   }
 
   const value = {
